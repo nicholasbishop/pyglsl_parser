c7424426fbdb51cad203c0990fd7e4fa23e7a05e
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-23 03:13:47 -0400

    Add comments

--- example.py
+++ example.py
@@ -25,10 +25,14 @@ def main():
     path = cli_args.path
     shader_type = ShaderType[cli_args.shader_type]
 
+    # Read in the source code
     with open(path) as glsl_file:
         source = glsl_file.read()
 
+    # Parse the source code
     ast = parse(source, path, shader_type)
+
+    # Summarize
     print('functions:')
     for func in ast.functions:
         print(func)
